5bb9e3e5c9a41fab722a8c5db79e010dda54fc52
Jesse <86168807+J-Sallis@users.noreply.github.com>
2021-12-22 16:27:46 -0700

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/README-checkpoint.md
+++
@@ -1,4 +0,0 @@
-# NHL_stats
- Plotly Dash application for NHL statistics
-
-Deployed dashboard can be accessed here: https://nhl-stats-dash.herokuapp.com/
--- .ipynb_checkpoints/app-checkpoint.py
+++
@@ -1,5 +0,0 @@
-from jupyter_dash import JupyterDash
-import dash_bootstrap_components as dbc
-
-app = JupyterDash(__name__, external_stylesheets=[dbc.themes.CYBORG])
-server = app.server
